b14b4b14a34522783df0fdcb054982f369bbc9a6
joseboschero <joseboscherodev@gmail.com>
2023-07-18 00:36:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ThreadsLogo from "./components/ThreadsLogo";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-black">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
