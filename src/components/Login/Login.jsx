5ab1c682c35fd91accc84d37ef599c81d35eaae5
joseboschero <joseboscherodev@gmail.com>
2023-07-20 13:13:30 -0300

    added password viewButton

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,11 +7,30 @@ import axiosInstance from "../../utils/axiosInstance";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setAccessToken } from "../../redux/slices/auth";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import FilledInput from "@mui/material/FilledInput";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 function Login() {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState({ message: null, credentials: false });
+
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   const dispatch = useDispatch();
 
@@ -50,17 +69,33 @@ function Login() {
             />
           </div>
           <div className="py-1">
-            <TextField
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
+            <FormControl
               fullWidth
               size="small"
               variant="outlined"
               onChange={(event) => setPassword(event.target.value)}
-              error={error.credentials}
-            />
+            >
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
           </div>
           <div className="flex justify-center py-1">
             <Button variant="outlined" onClick={onSubmit}>
