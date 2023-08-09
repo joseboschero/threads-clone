4ad02ea739a0f09f62e2fc1e4c0f15b83467d5fa
joseboschero <joseboscherodev@gmail.com>
2023-08-09 19:53:39 -0300

    deleting hardcoding

--- src/components/Thread/index.jsx
+++ src/components/Thread/index.jsx
@@ -6,8 +6,64 @@ import SendOutlinedIcon from "@mui/icons-material/SendOutlined";
 import { AvatarGroup } from "@mui/material";
 import ProfileAvatar from "./ProfileAvatar";
 import Header from "./Header";
+import { DateTime } from "luxon";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
 
 export default function Thread({ data }) {
+  const fechaDb = dayjs(new Date(data.createdAt).toISOString());
+  const fechaActual = dayjs();
+
+  if (fechaActual.diff(fechaDb, "hour") - 3 > 24) {
+    console.log("Publicado hace 1 dia");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 48) {
+    console.log("Publicado hace 2 dias");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 72) {
+    console.log("Publicado hace 3 dias");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 96) {
+    console.log("Publicado hace 4 dias");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 120) {
+    console.log("Publicado hace 5 dias");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 144) {
+    console.log("Publicado hace 6 dias");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 168) {
+    console.log("Publicado hace 1 semanas");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 336) {
+    console.log("Publicado hace 2 semanas");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 504) {
+    console.log("Publicado hace 3 semanas");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 672) {
+    console.log("Publicado hace 1 mes");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 1344) {
+    console.log("Publicado hace 2 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 2016) {
+    console.log("Publicado hace 3 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 2688) {
+    console.log("Publicado hace 4 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 3360) {
+    console.log("Publicado hace 5 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 4032) {
+    console.log("Publicado hace 6 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 4704) {
+    console.log("Publicado hace 7 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 5376) {
+    console.log("Publicado hace 8 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 6048) {
+    console.log("Publicado hace 9 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 6720) {
+    console.log("Publicado hace 10 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 7392) {
+    console.log("Publicado hace 11 meses");
+  } else if (fechaActual.diff(fechaDb, "hour") - 3 > 8064) {
+    console.log("Publicado hace 1 año");
+  } else {
+    console.log("Publicado hace mas de 1 año");
+  }
+
+  console.log(fechaActual.diff(fechaDb, "hour") - 3);
+
   return (
     <div className="w-full mb-5 mt-2">
       <div className="w-full flex justify-center">
@@ -48,9 +104,11 @@ export default function Thread({ data }) {
             <Avatar alt="" src="Untitled.png" sx={{ width: 15, height: 15 }} />
           </AvatarGroup>
           <div>
-            <span className="text-slate-600 pl-4">5 respuestas</span>
+            <span className="text-slate-600 pl-4">
+              {data.Replies.length} respuestas
+            </span>
             <span className="text-slate-600 pl-2">-</span>
-            <span className="text-slate-600 pl-2">132 Me gusta</span>
+            <span className="text-slate-600 pl-2">{data.likes} Me gusta</span>
           </div>
         </div>
       </div>
