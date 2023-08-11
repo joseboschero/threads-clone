33f94676ac3df85137687878a8525d1c99cdf330
sonnyrmt <santi2357@hotmail.com>
2023-08-10 22:31:56 -0300

    refactor: format fn

--- src/utils/formatTimeDifference.js
+++ src/utils/formatTimeDifference.js
@@ -7,20 +7,19 @@ const intervals = [
 ];
 
 export default function formatTimeDifference(diffInHours) {
-  for (let i = 0; i < intervals.length; i++) {
-    const interval = intervals[i];
-    if (diffInHours < interval.limit) {
-      if (i === 0) {
-        return `${diffInHours}${interval.unit}`;
-      } else {
-        const prevInterval = intervals[i - 1];
-        const diffInPrevUnit = Math.floor(diffInHours / prevInterval.limit);
-        return `${diffInPrevUnit === 1 ? '1' : diffInPrevUnit}${prevInterval.unit}`;
-      }
-    }
+  const interval = intervals.find((interval) => diffInHours < interval.limit);
+
+  if (!interval) {
+    const lastInterval = intervals[intervals.length - 1];
+    const diffInLastUnit = Math.floor(diffInHours / lastInterval.limit);
+    return `${diffInLastUnit === 1 ? '1' : diffInLastUnit}${lastInterval.unit}`;
   }
 
-  const lastInterval = intervals[intervals.length - 1];
-  const diffInLastUnit = Math.floor(diffInHours / lastInterval.limit);
-  return `${diffInLastUnit === 1 ? '1' : diffInLastUnit}${lastInterval.unit}`;
+  if (interval === intervals[0]) {
+    return `${diffInHours}${interval.unit}`;
+  }
+
+  const prevInterval = intervals[intervals.indexOf(interval) - 1];
+  const diffInPrevUnit = Math.floor(diffInHours / prevInterval.limit);
+  return `${diffInPrevUnit === 1 ? '1' : diffInPrevUnit}${prevInterval.unit}`;
 }
